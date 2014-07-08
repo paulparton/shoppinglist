0c24b7b917cd68a58141789047b69a70442db830
paulparton <paulparton@gmail.com>
2014-07-08 22:39:39 +0000

    small changes

--- client/app/lists/lists.controller.js
+++ client/app/lists/lists.controller.js
@@ -3,17 +3,20 @@
 angular.module('shoprApp')
   .controller('ListsCtrl', function ($scope, lists) {
 
+  	//Add a new list
   	$scope.addList = function(){
 
+  		//Save the new list to the database
   		lists.save({name:$scope.newList},
 
+  			//If the save works
 			function(data){
 
-				console.log("found:");
-				console.log(data);
+				//Add the new list to the ui
+				$scope.lists.push(data);
+
+				//Clear the new list form
 				$scope.newList = "";
-				
-$scope.loadLists();
 
 			},
 
@@ -27,9 +30,26 @@ $scope.loadLists();
   	};
 
   	$scope.removeList = function(listId){
-  		console.log(listId);
+  		lists.remove(
+			{id:listId},
+						function(data){
+				$scope.lists = data;
+
+				console.log("found:");
+				console.log(data);
+
+				//$scope.lists.splice($scope.lists.indexOf(data), 1);
+
+
+			},
+			function(err){
+				console.log("error");
+				console.log(err);
+			}	
+		);
   	};
 
+  	//refresh lists from the server
   	$scope.loadLists = function(){
 	  	lists.query({}, 
 			function(data){
